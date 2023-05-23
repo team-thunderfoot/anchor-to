7840bf687b61971c5cdb0f98ac336c4a5298cd51
Julieta Fredes <julietal.fredes@gmail.com>
2023-05-23 11:43:25 +0200

    anchorTo

--- src/ScrollTo.js
+++ src/ScrollTo.js
@@ -1,9 +1,22 @@
 class ScrollTo {
-    constructor(){
-    }
-    init(){
-    }
-    events(){  }
+  constructor(payload) {
+    this.DOM = {
+      anchorTo: payload.anchorTo,
+    };
+    this.offsetTop = payload.offsetTop ? payload.offsetTop : 500;
+  }
+  init() {}
+
+  events() {}
+
+  anchorTo(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: this.DOM.anchorTo.offsetTop - this.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
 }
 
-export default ScrollTo;   
+export default ScrollTo;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+import ScrollTo from "./ScrollTo";
 
-import ScrollTo from './ScrollTo';
 class Page {
-    constructor(){
-        this.init()
-        this.events()
-    }
-    init(){ 
-    }
-    events(){}
+  constructor() {
+    this.init();
+    this.events();
+  }
+  init() {
+    this.scrollTo = new ScrollTo({
+      anchorTo: document.getElementById("title"),
+      offsetTop: 100,
+    });
+
+    document.querySelector(".js--anchor-to").addEventListener("click", (e) => {
+      this.scrollTo.anchorTo(e);
+    });
+  }
+
+  events() {}
 }
 export default Page;
-new Page()
+new Page();
