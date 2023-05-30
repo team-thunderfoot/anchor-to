7fadfe10724c9b527c233af4987f556ca19ef516
Julieta Fredes <julietal.fredes@gmail.com>
2023-05-30 16:05:05 +0200

    add comments

--- src/AnchorTo.js
+++ src/AnchorTo.js
@@ -1,32 +1,36 @@
 class AnchorTo {
   constructor(payload) {
     this.DOM = {
-      triggers: document.querySelectorAll(`.${payload.trigger}`) ?? [],
-      checkUrl: payload.checkUrl,
-      anchorTo: payload.anchorTo,
-      offsetTop: payload.offsetTop,
-      offsetTopURL: payload.offsetTopURL,
+      triggers: document.querySelectorAll(`.${payload.trigger}`) ?? [], // Selects all elements with the specified class in payload.trigger
+      checkUrl: payload.checkUrl, // Indicates whether to check the URL for scrolling
+      anchorTo: payload.anchorTo, // Attribute that contains the ID of the element to scroll to
+      offsetTop: payload.offsetTop, // Custom vertical offset
+      offsetTopURL: payload.offsetTopURL, // Custom vertical offset
     };
     this.init();
   }
 
   init() {
+    // Performs scrolling if a parameter is found in the URL
     if (this.DOM.checkUrl) {
       this.scrollFromURL();
     }
 
+    // Assigns click event to each trigger element
     if (!!this.DOM.triggers.length) {
       this.DOM.triggers.forEach((button) => {
-        const targetId = button.getAttribute(this.DOM.anchorTo);
-        const targetEl = document.getElementById(targetId);
+        const targetId = button.getAttribute(this.DOM.anchorTo); // Get the ID of the target element from the attribute specified in payload.anchorTo
+        const targetEl = document.getElementById(targetId); // Get the target element based on the obtained ID
 
+        // Check if a custom offset is specified
         if (this.DOM.offsetTop) {
           var offsetTop = button.getAttribute(this.DOM.offsetTop);
         }
 
+        // Add a click event listener to each trigger button
         button.addEventListener("click", (e) => {
           e.preventDefault();
-          this.scrollTo(targetEl.offsetTop, offsetTop);
+          this.scrollTo(targetEl.offsetTop, offsetTop); // Perform smooth scroll to the target element
         });
       });
     }
@@ -43,14 +47,13 @@ class AnchorTo {
   }
 
   scrollFromURL() {
-    const url = window.location.href;
-    const hashIndex = url.indexOf("?");
+    const url = window.location.href; // Get the current URL of the browser
+    const hashIndex = url.indexOf("?"); // Check if a parameter exists in the URL for scrolling
 
     if (hashIndex > 0) {
-      const targetId = url.slice(hashIndex + 1);
-      const targetEl = document.getElementById(targetId);
-      //TODO: Obtener distancia de algun Trigger
-      this.scrollTo(targetEl.offsetTop, this.DOM.offsetTopURL);
+      const targetId = url.slice(hashIndex + 1); // Get the ID of the target element from the URL
+      const targetEl = document.getElementById(targetId); // Get the target element based on the obtained ID
+      this.scrollTo(targetEl.offsetTop, this.DOM.offsetTopURL); // Perform smooth scroll to the target element from the URL
     }
   }
 }
